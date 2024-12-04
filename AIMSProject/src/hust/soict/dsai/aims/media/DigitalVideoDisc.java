0ec1a8eed008b2141f8e0208b34ffd11d3ee5bff
Hoang Le <hoang.ln235498@sis.hust.edu.vn>
2024-12-04 21:00:31 +0700

    Update DigitalVideoDisc.java

--- AIMSProject/src/hust/soict/dsai/aims/media/DigitalVideoDisc.java
+++ AIMSProject/src/hust/soict/dsai/aims/media/DigitalVideoDisc.java
@@ -2,36 +2,35 @@ package hust.soict.dsai.aims.media;
 
 public class DigitalVideoDisc extends Disc implements Playable {
 
+	//constructors
 	public DigitalVideoDisc(String title) {
 		super();
-		this.title = title;
+		this.setTitle(title);
 	}
-	
 	public DigitalVideoDisc(String title, String category, float cost) {
 		super();
-		this.title = title;
-		this.category = category;
-		this.cost = cost;
+		this.setTitle(title);
+		this.setCategory(category);
+		this.setCost(cost);
 	}
 	public DigitalVideoDisc(String title, String category, String director, float cost) {
 		super();
-		this.title = title;
-		this.category = category;
-		this.director = director;
-		this.cost = cost;
+		this.setTitle(title);
+		this.setCategory(category);
+		this.setDirector(director);
+		this.setCost(cost);
 	}
 	public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
 		super();
-		this.title = title;
-		this.category = category;
-		this.director = director;
-		this.length = length;
-		this.cost = cost;
+		this.setTitle(title);
+		this.setCategory(category);
+		this.setDirector(director);
+		this.setLength(length);
+		this.setCost(cost);
 	}
 
 	public String toString() {
-	    return "DVD - " + title + " - " + category + " - " + director + 
-	           " - " + length + " - " + cost + "$";
+		return "DVD - " + this.getTitle() + " - " + this.getCategory() + " - " + this.getDirector() + " - " + Integer.toString(this.getLength()) + " - " + Float.toString(this.getCost()) + "$";
 	}
 
 	// Play method
